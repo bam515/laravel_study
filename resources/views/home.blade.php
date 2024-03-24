054f3772aeefc35508a23c398d4193e83570df8f
bam <devbam515@gmail.com>
2024-03-24 17:23:32 +0900

    login & logout 구현

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,14 @@
     <meta charset="utf-8">
     <title>첫 화면</title>
 </head>
-<body>
-<p>안녕하세요!</p>
+<p>
+<p>안녕하세요!
+    @if (Auth::check())
+        {{ \Auth::user()->name }}님</p>
+        <p><a href="/logout">로그아웃</a></p>
+    @else
+        게스트님</p>
+        <p><a href="/login">로그인</a><br><a href="/register">회원등록</a></p>
+    @endif
 </body>
 </html>
